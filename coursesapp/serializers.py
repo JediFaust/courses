00c3cf9dd220e0dfecc3daf53d5bfd83187e346e
JediFaust <skillerdoto@gmail.com>
2021-10-05 22:35:33 +0600

    third one

--- coursesapp/serializers.py
+++ coursesapp/serializers.py
@@ -2,28 +2,7 @@ from rest_framework import serializers
 from coursesapp.models import Course, LANGUAGE_CHOICES, STYLE_CHOICES
 
 
-class CourseSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    title = serializers.CharField(required=False, allow_blank=True, max_length=100)
-    description = serializers.CharField(style={'base_template': 'textarea.html'})
-    actual = serializers.BooleanField(required=False)
-    language = serializers.ChoiceField(choices=LANGUAGE_CHOICES, default='python')
-    style = serializers.ChoiceField(choices=STYLE_CHOICES, default='friendly')
-
-    def create(self, validated_data):
-        """
-        Create and return a new `Course` instance, given the validated data.
-        """
-        return Course.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        """
-        Update and return an existing `Course` instance, given the validated data.
-        """
-        instance.title = validated_data.get('title', instance.title)
-        instance.description = validated_data.get('description', instance.description)
-        instance.actual = validated_data.get('actual', instance.actual)
-        instance.language = validated_data.get('language', instance.language)
-        instance.style = validated_data.get('style', instance.style)
-        instance.save()
-        return instance
+class CourseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Course
+        fields = ['name', 'description', 'category', 'logo', 'contacts', 'branches']
